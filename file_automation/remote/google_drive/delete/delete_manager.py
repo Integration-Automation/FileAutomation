8a20875a50d805f3b54d19b122f3837b43655313
JE-Chen <zenxcvwait@gmail.com>
2023-06-05 03:32:17 +0800

    Add delete file
    Add delete file

---
+++ file_automation/remote/google_drive/delete/delete_manager.py
@@ -0,0 +1,12 @@
+from googleapiclient.errors import HttpError
+
+from file_automation.remote.google_drive.driver_instance import driver_instance
+
+
+def delete_file(file_id: str):
+    try:
+        file = driver_instance.service.files().delete(fileId=file_id).execute()
+        return file
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        return None
--- tests/unit_test/remote/google_drive/quick_test.py
+++ tests/unit_test/remote/google_drive/quick_test.py
@@ -1,3 +1,5 @@
-from file_automation.remote.google_drive.dir.folder_manager import add_folder
+from file_automation.remote.google_drive.search.search_drive import search_all_file
+from file_automation.remote.google_drive.delete.delete_manager import delete_file
 
-print(add_folder("new_folder"))
+print(search_all_file())
+print(delete_file("1x0HyWIXluTbyfo2UuQbpF62Ks8Dqz0Ud"))
