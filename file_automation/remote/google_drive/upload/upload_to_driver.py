7e9ced3ed38db29d6868436c19d7da303bbca5d0
JeffreyChen <33644111+JE-Chen@users.noreply.github.com>
2023-06-05 11:48:03 +0800

    Add download from folder
    Add download from folder

--- file_automation/remote/google_drive/upload/upload_to_driver.py
+++ file_automation/remote/google_drive/upload/upload_to_driver.py
@@ -8,10 +8,35 @@ from file_automation.remote.google_drive.driver_instance import driver_instance
 
 def upload_to_drive(file_name: str, file_path: str):
     try:
-        if Path(file_path).is_file():
+        if Path(file_path).exists():
             file_metadata = {
                 "name": file_name,
                 "mimeType": "*/*"
+            }
+            media = MediaFileUpload(
+                file_path,
+                mimetype="*/*",
+                resumable=True
+            )
+            file_id = driver_instance.service.files().create(
+                body=file_metadata,
+                media_body=media,
+                fields="id"
+            ).execute()
+            return file_id
+        return False
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return None
+
+
+def upload_to_dir(folder_id: str, file_name: str, file_path: str):
+    try:
+        if Path(file_path).exists():
+            file_metadata = {
+                "name": file_name,
+                "mimeType": "*/*",
+                "parents": [f"{folder_id}"]
             }
             media = MediaFileUpload(
                 file_path,
--- tests/unit_test/remote/google_drive/quick_test.py
+++ tests/unit_test/remote/google_drive/quick_test.py
@@ -1,5 +1,5 @@
+from file_automation.remote.google_drive.download.download_file import download_file_from_folder
 from file_automation.remote.google_drive.search.search_drive import search_all_file
-from file_automation.remote.google_drive.share.share_file import share_file_to_anyone
 
 print(search_all_file())
-print(share_file_to_anyone("1PlUh-iBEQF4n-0Ew0FfabVuoDZbuFpxc"))
+print(download_file_from_folder("new_folder"))
