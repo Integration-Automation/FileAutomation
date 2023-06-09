1a16c9f82f692397eaec5d25fa64a8072f2f1d93
JeffreyChen <33644111+JE-Chen@users.noreply.github.com>
2023-06-09 11:58:05 +0800

    Add command to executor
    Add command to executor

--- file_automation/utils/callback/callback_function_executor.py
+++ file_automation/utils/callback/callback_function_executor.py
@@ -1,25 +1,23 @@
 import typing
 from sys import stderr
 
-from file_automation.utils.exception.exception_tags import get_bad_trigger_function, get_bad_trigger_method
-from file_automation.utils.exception.exceptions import CallbackExecutorException
-
+from file_automation.local.dir.dir_process import copy_dir, create_dir, remove_dir_tree
 from file_automation.local.file.file_process import copy_file, remove_file, rename_file, copy_specify_extension_file, \
     copy_all_file_to_dir
-from file_automation.local.dir.dir_process import copy_dir, rename_dir, create_dir, remove_dir_tree
 from file_automation.local.zip.zip_process import zip_dir, zip_file, zip_info, zip_file_info, set_zip_password, \
     read_zip_file, unzip_file, unzip_all
-
+from file_automation.remote.google_drive.delete.delete_manager import delete_file
+from file_automation.remote.google_drive.dir.folder_manager import add_folder
+from file_automation.remote.google_drive.download.download_file import download_file, download_file_from_folder
 from file_automation.remote.google_drive.driver_instance import driver_instance
 from file_automation.remote.google_drive.search.search_drive import \
     search_all_file, search_field, search_file_mimetype
-from file_automation.remote.google_drive.upload.upload_to_driver import \
-    upload_dir_to_folder, upload_to_folder, upload_dir_to_drive, upload_to_drive
-from file_automation.remote.google_drive.dir.folder_manager import add_folder
 from file_automation.remote.google_drive.share.share_file import \
     share_file_to_anyone, share_file_to_domain, share_file_to_user
-from file_automation.remote.google_drive.delete.delete_manager import delete_file
-from file_automation.remote.google_drive.download.download_file import download_file, download_file_from_folder
+from file_automation.remote.google_drive.upload.upload_to_driver import \
+    upload_dir_to_folder, upload_to_folder, upload_dir_to_drive, upload_to_drive
+from file_automation.utils.exception.exception_tags import get_bad_trigger_function, get_bad_trigger_method
+from file_automation.utils.exception.exceptions import CallbackExecutorException
 
 
 class CallbackFunctionExecutor(object):
@@ -45,7 +43,18 @@ class CallbackFunctionExecutor(object):
             "driver_instance": driver_instance,
             "search_all_file": search_all_file,
             "search_field": search_field,
-            "search_file_mimetype": search_file_mimetype
+            "search_file_mimetype": search_file_mimetype,
+            "upload_dir_to_folder": upload_dir_to_folder,
+            "upload_to_folder": upload_to_folder,
+            "upload_dir_to_drive": upload_dir_to_drive,
+            "upload_to_drive": upload_to_drive,
+            "add_folder": add_folder,
+            "share_file_to_anyone": share_file_to_anyone,
+            "share_file_to_domain": share_file_to_domain,
+            "share_file_to_user": share_file_to_user,
+            "delete_file": delete_file,
+            "download_file": download_file,
+            "download_file_from_folder": download_file_from_folder
         }
 
     def callback_function(
